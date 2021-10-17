9c0cf696a25471625c5793df12456484aae0f0a9
Sarwar-95 <sarwarahmad164@gmail.com>
2021-10-18 04:08:09 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="main-header">
+                <h1 className="heading">Top Project Team</h1>
+                <p>Top github contributors from Bangladesh to work on your project!</p>
+                <h2>Budget : $500000</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
